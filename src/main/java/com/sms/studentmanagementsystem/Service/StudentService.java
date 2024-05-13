2563da02e210c086665c1788c8c2245b0bcda19e
Kavinda Botheju <117912732+KJBotheju@users.noreply.github.com>
2024-05-13 22:54:40 +0530

    packages

---
+++ src/main/java/com/sms/studentmanagementsystem/Service/StudentService.java
@@ -0,0 +1,5 @@
+package com.sms.studentmanagementsystem.Service;
+
+public interface StudentService {
+
+}
---
+++ src/main/java/com/sms/studentmanagementsystem/Service/StudentServiceImpl.java
@@ -0,0 +1,5 @@
+package com.sms.studentmanagementsystem.Service;
+
+public class StudentServiceImpl implements StudentService{
+
+}
